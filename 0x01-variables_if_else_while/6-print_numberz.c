3f1a17f8ca484b49629716b6a37378a58dfa8b1f
Lanre-Awe <lanreawe7@gmail.com>
2022-09-09 15:40:50 -0700

    base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(%i)
+	}
+	putchar('\n')
+}
