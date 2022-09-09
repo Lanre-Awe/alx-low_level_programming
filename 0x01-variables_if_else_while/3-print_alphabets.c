6d8999585a632e65bf99fdf07bb02472f0943158
Lanre-Awe <lanreawe7@gmail.com>
2022-09-09 14:01:45 -0700

    two cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry pint
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int ch, uc
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (uc = 'A'; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+	putchar('\n')
+	return (0);
+}
