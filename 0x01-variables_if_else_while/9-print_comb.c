db61eee11a96889277fbc41471b2c8199b77f6d0
Lanre-Awe <lanreawe7@gmail.com>
2022-09-09 22:39:05 -0700

    Add new c scripts

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,34 +1,26 @@
 #include <stdio.h>
 
-
-
 /**
- *	main - Entry point
+ *	main - prints all possible combinations of single-digit numbers
+ *
  *	Return: Always 0 (Success)
+ *
  */
 
 int main(void)
-
 {
-
 	int n;
-
-	char ch;
 
 	for (n = 48; n < 58; n++)
 	{
-
 		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-
-	for (ch = 'a'; ch <= 'f'; ch++)
-
-	{
-		putchar(ch);
-	}
-
-	putchar ('\n');
-
+	putchar('\n');
 	return (0);
-
 }
+
