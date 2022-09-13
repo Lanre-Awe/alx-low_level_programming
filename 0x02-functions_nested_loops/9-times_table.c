155c4bca35d87aeb081bb1573bbaa81a6b75d660
Lanre-Awe <lanreawe7@gmail.com>
2022-09-13 15:44:21 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,15 +15,25 @@ void times_table(void)
 		for (k = 0; k <= 9; k++)
 		{
 			j = i * k;
-			if ((j / 10) > 0)
+			if (k == 0)
 			{
-				_putchar((j / 10) + '0');
+				_putchar((j + '0');
 			}
-			_putchar((j % 10) + '0');
-			if (k < 9)
+			else if (j >= 10)
+			{
+				_putchar(' ');
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(j + '0');
+			}
+			if (k != 9)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
