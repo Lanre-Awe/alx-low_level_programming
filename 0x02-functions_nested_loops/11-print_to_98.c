7156b5befdf543bd4d89654277d74c0a4c1106c0
Lanre-Awe <lanreawe7@gmail.com>
2022-09-13 16:08:45 -0700

    count to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all antural numbers from n to 98
+ *
+ * @n: type int
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	}
+}
+
