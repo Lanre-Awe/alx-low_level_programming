fa54ab84aa1236eb6ea9a4280e9bf36e4b7db225
Lanre-Awe <lanreawe7@gmail.com>
2022-09-15 04:31:06 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,6 @@
 
 int largest_number(int a, int b, int c)
 {
-
 	int largest;
 
 	if (a > b && a > c)
@@ -21,7 +20,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
 	}
