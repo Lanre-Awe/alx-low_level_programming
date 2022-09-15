8b1e6f125105270aa26202ac6c22f753da02ab92
Lanre-Awe <lanreawe7@gmail.com>
2022-09-15 13:58:21 -0700

    draw a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_line - prints a straight line in the terminal
+ * print_line - prints a straight line
  * @n: type int
  */
 
@@ -10,7 +10,7 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	} else
 	{
 		int j;
 
