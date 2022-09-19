877199fc49449d6340a7113c0013843131b71413
Lanre-Awe <lanreawe7@gmail.com>
2022-09-19 13:11:36 -0700

    get string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - gets the length of the string
+ * @s: type char
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; ++len)
+	{
+		return (len);
+	}
+}
