b7adf6016396dd5da6c24303cd9364dc194308f9
Lanre-Awe <lanreawe7@gmail.com>
2022-09-19 13:38:01 -0700

    gnirts esrever

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints string in reverse
+ * @s: type char
+ * return: void
+ */
+
+
+
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
