9bce43e2b81419fc82f5244943bb5e289a3ee2bc
Lanre-Awe <lanreawe7@gmail.com>
2022-09-22 07:57:40 -0700

    added tasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n ; type int
+ */
+
+void print_number(int n)
+{
+	unsigned int a1;
+
+	if (n < 0)
+	{
+		a1 = -n;
+		_putchar('-');
+	} else
+	{
+		a1 = n;
+	}
+
+	if (a1 / 10)
+		print_number(a1 / 10);
+
+	_putchar((a1 % 10) + '0');
+}
