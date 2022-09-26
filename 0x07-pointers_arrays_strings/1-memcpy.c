430b6ab4f5eb99a8f41b6067f5320db69a5f64f1
Lanre-Awe <lanreawe7@gmail.com>
2022-09-26 13:58:35 -0700

    added tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ *
+ * @dest: type char
+ * @src: type char
+ * @n: type int
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - locates a character in a string
+ * @s: type char
+ * @c: type char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	char *p;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] == 'c')
+		{
+			return (p);
+		} else
+		{
+			return NULL;
+		}
+	}
+}
