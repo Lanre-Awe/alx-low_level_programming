4cc64ad57097a87f0e14c6aa6ae87bdf29cfcb01
Lanre-Awe <lanreawe7@gmail.com>
2022-09-30 13:31:11 -0700

    task added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - prints its name
+ * @argc: the number of arguments
+ * @argv: an array of string containing the arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+	_putchar('\n');
+
+	return (0);
+}
