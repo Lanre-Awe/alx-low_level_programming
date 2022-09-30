80ca6318c98d405aba0d63481a8b755c7195f723
Lanre-Awe <lanreawe7@gmail.com>
2022-09-30 14:31:30 -0700

    task added

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguement
+ * @argv: arguments
+ * Return: error , 1 or 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int j = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (int i = 1; i <= argc; i++)
+	{
+		j *= *argv[i];
+	}
+	return (0);
+}
