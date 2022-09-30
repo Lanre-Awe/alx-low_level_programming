6550d18550706052e8fe597e6a693da8b560fdfa
Lanre-Awe <lanreawe7@gmail.com>
2022-09-30 14:13:28 -0700

    task added

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,9 +11,12 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	if (argc > 0)
 	{
-		printf("%s\n", *argv[i])
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", *argv[i])
+		}
 	}
 	return (0);
 }
