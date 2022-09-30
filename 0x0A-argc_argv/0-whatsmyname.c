1de548858585f20c7cd54f7f16bbee5d5f9cce55
Lanre-Awe <lanreawe7@gmail.com>
2022-09-30 13:49:20 -0700

    task added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-
 /**
  * main - prints its name
  * @argc: the number of arguments
@@ -12,6 +11,5 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-
 	return (0);
-}	
+}
