d9916593ec0e2fbee356ea52112bdc72e614e646
Lanre-Awe <lanreawe7@gmail.com>
2022-10-13 19:09:39 -0700

    added task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,8 +17,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	for (i = n; i >= 0 || i < 0; i = va_arg(ap, int))
+	for (i = n; i >= 0 || i <= 0; i = va_arg(ap, int))
 	{
+		if (n == 0)
+		{
+			return (0);
+		}
 		sum += i;
 	}
 	va_end(ap);
